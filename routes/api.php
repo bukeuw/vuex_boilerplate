05519e411b58f3ae590b379a6c10506411c06279
= <=>
2017-03-07 17:40:06 +0700

    Add logout functionality

--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,8 @@ use Illuminate\Http\Request;
 
 Route::post('/register', 'Auth\AuthController@register');
 Route::post('/login', 'Auth\AuthController@login');
+Route::post('/logout', 'Auth\AuthController@logout');
+
+Route::group(['middleware' => 'jwt.auth'], function () {
+    Route::get('me', 'Auth\AuthController@user');
+});
