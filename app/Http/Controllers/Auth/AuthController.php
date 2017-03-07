ebefa4930e721fe44a6b84935eb1f5e4fdf61be8
= <=>
2017-03-07 16:56:00 +0700

    Add register functionality

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\User;
+use Tymon\JWTAuth\JWTAuth;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\RegisterFormRequest;
+
+class AuthController extends Controller
+{
+    public function __construct(JWTAuth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function register(RegisterFormRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $token = $this->auth->attempt($request->only(['email', 'password']));
+
+        return response()->json([
+            'data' => $user,
+            'meta' => [
+                'token' => $token
+            ]
+        ], 200);
+    }
+}
