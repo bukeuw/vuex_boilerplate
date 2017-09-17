23d0c10c1755d31af972cfac622ddeb46080eef2
Bu Keuw <sntblasius@gmail.com>
2017-09-17 09:47:44 +0700

    Add basic frontend project structure

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,7 +13,8 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example', require('./components/Example.vue'));
+Vue.component('app', require('./components/App.vue'));
+Vue.component('navigation', require('./components/Navigation.vue'));
 
 const app = new Vue({
     el: '#app'
---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <navigation></navigation>
+
+        <div class="container">
+            Router view
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            //console.log('Component mounted.')
+        }
+    }
+</script>
